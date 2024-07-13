2e54e0c0a988c596a3d948e33300f0efc6a16e3c
Nikita <nikita_bulak@mail.ru>
2024-07-13 09:58:11 +0300

    Lesson №12 add postgres (docker-compose), liquibase, spring data, audit fields for entities, filter search

--- src/main/java/ru/bulak/linkshortener/repository/LinkInfoRepository.java
+++ src/main/java/ru/bulak/linkshortener/repository/LinkInfoRepository.java
@@ -1,17 +1,44 @@
 package ru.bulak.linkshortener.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.bulak.linkshortener.model.LinkInfo;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface LinkInfoRepository {
-    Optional<LinkInfo> findByShortLink(String shortLink);
+@Repository
+public interface LinkInfoRepository extends JpaRepository<LinkInfo, UUID> {
 
-    LinkInfo saveShortLink(LinkInfo linkInfo);
+    @Query("""
+            FROM LinkInfo li
+            WHERE (:linkPart IS NULL OR li.link LIKE '%' || :linkPart || '%')
+            AND (cast(:endTimeFrom as date) IS NULL OR li.endTime >= :endTimeFrom)
+            AND (cast(:endTimeTo as date) IS NULL OR li.endTime >= :endTimeTo)
+            AND (:descriptionPart IS NULL OR li.description LIKE '%' || :descriptionPart || '%')
+            AND (:active IS NULL OR li.active = :active)
+            """)
+    List<LinkInfo> findByFilter(String linkPart,
+                                ZonedDateTime endTimeFrom,
+                                ZonedDateTime endTimeTo,
+                                String descriptionPart,
+                                Boolean active);
 
-    List<LinkInfo> findAll();
+    Optional<LinkInfo> findByShortLinkAndActiveTrue(String shortLink);
 
-    boolean deleteById(UUID id);
+    Optional<LinkInfo> findByIdAndActiveTrue(UUID id);
+
+    @Query("""
+            UPDATE LinkInfo li
+            SET li.openingCount = li.openingCount + 1
+            WHERE li.shortLink = :shortLink
+                        """)
+    @Modifying
+    @Transactional
+    void incrementOpeningCountByShortLink(String shortLink);
 }
